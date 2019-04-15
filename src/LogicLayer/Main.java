49498e003825151f51925f21f9c605d07831e827
Elad Feldman <eladfeld@post.bgu.ac.il>
2019-04-15 17:23:06 +0300

    build new class path and update main

--- src/LogicLayer/Main.java
+++ src/LogicLayer/Main.java
@@ -3,7 +3,10 @@ package LogicLayer;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(123);
+		RationalScalar myDick1 = new RationalScalar(-1,2);
+		System.out.println(myDick1);
+		RationalScalar myDick2 =(RationalScalar) myDick1.add(myDick1);
+		System.out.println(myDick2);
 	}
 
 }
